3c4dd283c141920a302a5e08ad940c1adbca9eda
qugemin <20020116@outlook.com>
2024-01-06 15:46:17 +0800

    6

--- CQIE.OnlineVote.Models/SysRole.cs
+++ CQIE.OnlineVote.Models/SysRole.cs
@@ -9,7 +9,7 @@ namespace CQIE.OnlineVote.Models
     public class SysRole
 
     {  
-        //好的，已经修改
+        //好的，已经修改完了
         public int Id { get; set; }
         public string? RoleName { get; set; }
         public string? Describe { get; set;}
